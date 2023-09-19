a52d65782b7e33d4ecf9bff106fb20b18700a9e1
Elke Grau <139354220+elkegrau-cbe@users.noreply.github.com>
2023-09-19 13:07:28 +0200

    feat: add tasks variables

---
+++ tasks/04-variables/script.js
@@ -0,0 +1,38 @@
+// Spot the errors and fix them Copy the following complete code block to your script.js file and try to spot and fix the errors.
+
+let userName = "Brad";
+
+function getUserNameLength() {
+  return userName;
+}
+console.log("Brad".length >= 4);
+
+userName = "Jenna";
+
+function getUserNameLength() {
+  return userName;
+}
+console.log("Jenna".length > 4);
+
+// ^______________ Should log true
+
+// TODO: Implement the isString function
+
+const isString = function (any) {
+  if (typeof any === "string") {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined)); // result should be false
+
+console.log(isString("")); // result should be true
+
+console.log(isString("John" + "Doe")); // result should be true
