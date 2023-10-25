e3cc7b06b8833a4c35ece1592fc1b8bcb10cfebd
Elke Grau <139354220+elkegrau-cbe@users.noreply.github.com>
2023-10-25 17:26:16 +0200

    feat: add task variables

--- tasks/04-variables/script.js
+++ tasks/04-variables/script.js
@@ -11,10 +11,31 @@ console.log("Jenna".length > 4);
 // ^______________ Should log true
 
 // TODO: Implement the isString function
-
+/*
 function isString(any) {
   return typeof any === "string";
 }
+
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined)); // result should be false
+
+console.log(isString("")); // result should be true
+
+console.log(isString("John" + "Doe")); // result should be true
+*/
+
+const isString = function (any) {
+  if (typeof any === "string") {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 console.log(isString("Hello"));
 // result should be true
